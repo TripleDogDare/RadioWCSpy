28db983c76686348b1ef7634b1f482102cbeb6fb
Calvin Behling <behl0043@umn.edu>
2015-04-27 01:46:24 +0000

    add first api call

--- www/test/unit/controllersSpec.js
+++ www/test/unit/controllersSpec.js
@@ -8,13 +8,16 @@ describe('Radio WCS controllers', function() {
 
     beforeEach(module('radiowcsApp'));
 
-    beforeEach(inject(function($controller) {
-      scope = {};
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+	$httpBackend.expectGet('api/recent');
+	respond([{artist:'Selena', title: 'test', bpm: 32, image: '', date: 123456789}]);
+	scope = $rootScope.$new();
       ctrl = $controller('PlaylistCtrl', {$scope:scope});
     }));
 
     it('should create "playlist" model with specified # of tracks', function() {
-      expect(scope.playlist.length).toBe(20);
+      expect(scope.playlist.length).toBe(1);
     });
 
 
